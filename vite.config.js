852bf17e5de5fc2022aa515e1eb12dc9cbca05f9
Bernhard Hustomo <bernhard.hustomo@gmail.com>
2022-12-20 01:28:24 +0800

    append tailwind.config.js content into vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,27 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  content: ["*.{html,js}"],
+  theme: {
+    fontFamily: {
+      'eaves': ['mrs-eaves-roman-lining', 'serif']
+    },
+    borderRadius: {
+      '6xl': '3rem'
+    },
+    boxShadow: {
+      'button-shadow': '4px 4px 1px -1px #A89E93'
+    },
+    extend: {
+      backgroundImage: {
+        'hero': "url('/assets/images/hero.jpg')"
+      },
+      colors: {
+        'beige': '#F3EFE6',
+        'button-text': '#946B4D',
+        'button-shadow': '#A89E93'
+      },
+    },
+  },
   plugins: [react()],
 })
